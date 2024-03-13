4a884c27d5eccf6ea6aae7a967aa1ddc9bf7fce6
AlejandroJorge <a20202060@pucp.edu.pe>
2024-03-13 01:22:36 -0500

    Added theming

---
+++ src/config/theme.ts
@@ -0,0 +1,46 @@
+interface ITheme {
+  color: {
+    dark: IColorTheme;
+    light: IColorTheme;
+  };
+}
+
+interface IColorTheme {
+  text: string;
+  primary: string;
+  secondary: string;
+  background: string;
+  foreground: string;
+  strongerBackground: string;
+}
+
+const theme: ITheme = {
+  color: {
+    dark: {
+      text: "#fff",
+      primary: "#8f61d5",
+      secondary: "#607cd1",
+      background: "#0f1724",
+      foreground: "#929daf",
+      strongerBackground: "#000",
+    },
+    light: {
+      text: "#000",
+      primary: "#007bff",
+      secondary: "#6c757d",
+      background: "#f8f9fa",
+      foreground: "#333333",
+      strongerBackground: "#e0e1e2",
+    },
+  },
+};
+
+export function getCSSVars(mode: "light" | "dark") {
+  const colorTheme = theme.color[mode];
+  const cssVars = {};
+  for (const key in colorTheme) {
+    //@ts-ignore
+    cssVars[`${key}Color`] = colorTheme[key];
+  }
+  return cssVars;
+}
