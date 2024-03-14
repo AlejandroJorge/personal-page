f5ffb911994574b2ee6896abe2fe2769813da1e5
AlejandroJorge <a20202060@pucp.edu.pe>
2024-03-14 00:51:08 -0500

    Added footer and componetized

---
+++ src/config/info.ts
@@ -0,0 +1,7 @@
+export const portfolioGithubRepoLink =
+  "https://github.com/AlejandroJorge/personal-page";
+
+export const githubProfileLink = "https://github.com/AlejandroJorge/";
+
+export const linkedinProfileLink =
+  "https://www.linkedin.com/in/jorgealejandroc/";
